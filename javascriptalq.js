79229cf26ec46e085ff6b51a8fa3692598fe96c2
cristianpedrini <166650458+cristianpedrini@users.noreply.github.com>
2024-05-28 17:44:36 -0300

    Trabajo final actualizacion

---
+++ javascriptalq.js
@@ -0,0 +1,11 @@
+
+const distancia=41;
+
+function pregunta() {
+    var lugar= prompt("¿Cuál es la distancia en kilometros desde San Nicolás al destino?");
+    if (lugar<distancia) {
+    alert("El envío es gratis")
+    } else {alert("El envío sale $8.000")
+        
+    }}
+
--- javascripts.js
+++ javascripts.js
@@ -1,2 +1,6 @@
-alert ("Hola mundo")
+const grande= 18 ;
+let edad= window.prompt ("Cuantos años tenés?");
+ if (edad<grande) {
+    alert("ENTRA BAJO SU RESPONSABILIDAD")
+    }else{alert("BIENVENIDO") } 
     
